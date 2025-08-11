9579b4bd1d5f8ca614e84aeb45a2e68ab51cd9ef
Matheus <matheus271112@gmail.com>
2025-08-11 09:50:29 -0300

    Erro 404

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,39 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  async redirects() {
+    return [
+      {
+        source: '/servios',
+        destination: '/',
+        permanent: true,
+      },
+      {
+        source: '/catlogo',
+        destination: '/',
+        permanent: true,
+      },
+         {
+        source: '/sobre',
+        destination: '/a-pousada',
+        permanent: true,
+      },
+         {
+        source: '/perguntas-frequentes',
+        destination: '/',
+        permanent: true,
+      },
+         {
+        source: '/contato',
+        destination: '/contato',
+        permanent: true,
+      },
+        {
+        source: '/depoimentos',
+        destination: '/',
+        permanent: true,
+      },
+    ];
+  },
+};
 
 export default nextConfig;
